c02b66c5bd58bbb2c6366d5d605a4642bc2b3586
Unai <unai_abasolo@euneiz.com>
2026-05-03 22:03:34 +0200

    .

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ import sys, os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from flask import Flask, send_from_directory
-from backend.routes.auth_routes    import auth_bp
-from backend.routes.public_routes  import public_bp
+from backend.routes.auth_routes import auth_bp
+from backend.routes.public_routes import public_bp
 from backend.routes.reservation_routes import res_bp
-from backend.routes.notice_routes  import notice_bp
-from backend.routes.admin_routes   import admin_bp
-from backend.routes.bono_routes    import bono_bp
+from backend.routes.notice_routes import notice_bp
+from backend.routes.admin_routes import admin_bp
+from backend.routes.bono_routes import bono_bp
 
 app = Flask(
     __name__,
@@ -21,6 +21,14 @@ app = Flask(
 )
 app.secret_key = "euskomove-secret-2025-eus"
 
+
+@app.context_processor
+def inject_current_user():
+    def current_user():
+        return None
+    return dict(current_user=current_user)
+
+
 app.register_blueprint(public_bp)
 app.register_blueprint(auth_bp)
 app.register_blueprint(res_bp)
@@ -28,6 +36,7 @@ app.register_blueprint(notice_bp)
 app.register_blueprint(admin_bp)
 app.register_blueprint(bono_bp)
 
+
 if __name__ == "__main__":
-    print("\n✅  EuskoMove en http://127.0.0.1:3000\n")
+    print("\n  EuskoMove en http://127.0.0.1:3000\n")
     app.run(debug=True, port=3000)
